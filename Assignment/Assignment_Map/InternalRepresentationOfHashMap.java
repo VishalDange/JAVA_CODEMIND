de68fa4ba7726bed5b911230c1ddfae32eabcbbe
Vishal Dange <108215773+VishalDange@users.noreply.github.com>
2023-08-18 13:11:08 +0530

    Update InternalRepresentationOfHashMap.java

--- Assignment/Assignment_Map/InternalRepresentationOfHashMap.java
+++ Assignment/Assignment_Map/InternalRepresentationOfHashMap.java
@@ -5,37 +5,7 @@ import java.util.Map;
 public class InternalRepresentationOfHashMap 
 {
 	public static void main(String[] args) 
-	{
-//		ArrayList<String> mh=new ArrayList<>();
-//		mh.add("Pune");
-//		mh.add("Kolhapur");
-//		
-//		ArrayList<String> up=new ArrayList<>();
-//		mh.add("Mathura");
-//		mh.add("Agra");
-//		
-//		Map<String,ArrayList<String>> ind=new HashMap<>();
-//		ind.put("Maharashtra",mh);
-//		ind.put("Utter Pradesh",up);
-//		
-//		
-//		ArrayList<String> florida=new ArrayList<>();
-//		florida.add("Central florida");
-//		florida.add("baker");
-//		
-//		ArrayList<String> calofornia=new ArrayList<>();
-//		calofornia.add("san Jose");
-//		calofornia.add("san Francisco");
-//		
-//		Map<String,ArrayList<String>> us=new HashMap<>();
-//		ind.put("Maharashtra",florida);
-//		ind.put("Utter Pradesh",calofornia);
-//		
-//		Map<String,Map<String,ArrayList<String>>> world=new HashMap<>();
-//        world.put("India", ind);
-//        world.put("United State", us);
-////        world.put("India", ind);
-		
+	{	
 		ArrayList<String> mh = new ArrayList<>();
 		mh.add("Pune");
 		mh.add("Kolhapur");
