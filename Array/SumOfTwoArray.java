998d87cbc6dd4b4c5c9c9a31e6ee58b5cbe74ac2
vishaldange <vishaldange2002@gmail.com>
2025-04-06 22:44:29 +0530

    Practicing the github integration with the Intellij

--- Array/SumOfTwoArray.java
+++ Array/SumOfTwoArray.java
@@ -4,6 +4,8 @@ public class SumOfTwoArray
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+
         System.out.println("Enter the number of elements: ");
         int n = scanner.nextInt();
 
